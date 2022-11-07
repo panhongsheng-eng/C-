7ce8b735434a16a622f79ae35200a14c61bb8fb9
HUAWEI-Developer <79436937+panhongsheng-eng@users.noreply.github.com>
2022-11-07 21:47:43 +0800

    Create 3.害死人不偿命的（3n+1）猜想.c

---
+++ code/3.害死人不偿命的（3n+1）猜想.c
@@ -0,0 +1,10 @@
+void Pat_B1001() {
+    int n, step = 0;
+    scanf("%d", &n);
+    while (n != 1) {
+        if (n % 2 == 0) n = n / 2;
+        else n = (3 * n + 1) / 2;
+        step++;
+    }
+    printf("%d\n", step);
+}
